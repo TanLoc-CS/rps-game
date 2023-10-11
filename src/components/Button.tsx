bfdb52459947d28c063bd015b9621c253f4d710e
talu <talu.dev@gmail.com>
2023-10-11 13:09:21 +1100

    feat: handle usernameInput and navigate to InsPage

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,13 +10,19 @@ const Button = (props: buttonProps) => {
 
 	if (size === "normal") {
 		return (
-			<button className="w-[480px] h-[60px] hover:bg-neutral-700 focus:bg-black text-white bg-black rounded-lg">
+			<button
+				type="submit"
+				className="w-[480px] h-[60px] hover:bg-neutral-700 focus:bg-black text-white bg-black rounded-lg"
+			>
 				{name}
 			</button>
 		);
 	}
 	return (
-		<button className="w-[120px] h-[40px] hover:bg-neutral-700 focus:bg-black text-white bg-black rounded-lg">
+		<button
+			type="submit"
+			className="w-[120px] h-[40px] hover:bg-neutral-700 focus:bg-black text-white bg-black rounded-lg"
+		>
 			{name}
 		</button>
 	);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "components/Button";
 
 const HomePage = () => {
+	const [username, setUsername] = useState("");
+	const navigate = useNavigate();
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+		e.preventDefault();
+		sessionStorage.setItem("username", username);
+		navigate("/instruction", { replace: true });
+	};
+
 	return (
 		<div className="w-full md:h-full md:p-8 flex flex-col">
 			<h1 className="text-xl font-semibold">/Rock Paper Scissors/</h1>
 			<div className="w-full h-[600px] flex flex-row justify-center">
-				<form className="md:w-[560px] md:h-[480px] mt-[200px] p-10 flex flex-col justify-start items-start shadow-2xl rounded-lg">
+				<form
+					className="md:w-[560px] md:h-[480px] mt-[200px] p-10 flex flex-col justify-start items-start shadow-2xl rounded-lg"
+					onSubmit={handleSubmit}
+				>
 					<p className="text-2xl">Welcome!</p>
 					<div className="my-7">
 						<h2 className="text-[32px] font-semibold">/Rock Paper Scissors/</h2>
@@ -19,6 +31,8 @@ const HomePage = () => {
 							type="text"
 							placeholder="Enter your username"
 							className="w-[480px] h-[60px] px-[16px] text-[16px] border border-black focus-within:outline-none rounded-lg"
+							id="usernameInput"
+							onChange={(e) => setUsername(e.target.value)}
 						/>
 					</div>
 					<Button size="normal" name="Login" />
